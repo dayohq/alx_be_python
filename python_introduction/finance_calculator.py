169ac007ee78f9972c723a5db54ef152389029a7
dayohq <smartjoe59@gmail.com>
2024-12-21 00:05:47 +0100

    Used user input, variables, and arithmetic operations to calculate and provide feedback on a user’s monthly savings and potential future savings without applying conditional statements.

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,10 @@
+# Prompt the user to input their financial details
+monthly_income = int(input("Enter your monthly income"))
+monthly_expenses = int(input("Enter your total monthly expenses"))
+# Calculate Monthly Savings
+monthly_savings = monthly_income - monthly_expenses
+# Project Annual Savings
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+#Output Results
+print(monthly_savings)
+print(projected_savings)
