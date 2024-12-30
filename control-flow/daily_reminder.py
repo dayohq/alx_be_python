e354bb517d057694c66527fda6c319b565fc16f5
dayohq <smartjoe59@gmail.com>
2024-12-30 19:22:56 +0100

    correction 2 to personal daily reminder file.

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,21 +1,21 @@
-task = input("Enter task description: ")
-priority = input("Enter the task priority (high, medium, low): ")
-time_bound = input("Is task time-bound? (yes or no): ")
+Task = input("Enter task description: ")
+Priority = input("Enter the task priority (high, medium, low): ")
+Time_Bound = input("Is task time-bound? (yes or no): ")
 
 
-match priority:
+match Priority:
     case "high":
-        reminder = f"The task {task} is of high priority."
+        reminder = f"The task {Task} is of high priority."
     case "medium":
-        reminder = f"The task {task} is of medium priority."
+        reminder = f"The task {Task} is of medium priority."
     case "low":
-        reminder = f"The task {task} is of low priority."
+        reminder = f"The task {Task} is of low priority."
     case _:
-        reminder = f"The task {task} has an unspecified priority."
+        reminder = f"The task {Task} has an unspecified priority."
     
-if time_bound == "yes":
+if Time_Bound == "yes":
     reminder += " It requires immediate attention today!"
-elif time_bound != "no":
+elif Time_Bound != "no":
     print("\nInvalid time-bound input. Please enter yes or no.")
     reminder = "No reminder generated due to invalid input."
 
