c1b00957c6e9c2dab048f9841d6d17ef1a142042
dayohq <smartjoe59@gmail.com>
2024-12-30 09:48:18 +0100

    second correction to weather advice file

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,17 +1,12 @@
-#define weather variable
-weather1 = "sunny"
-weather2 = "rainy"
-weather3 = "cold"
-
 # user input
-current_weather = input("What's the weather like today? (sunny/rainy/cold): ")
+weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
 # clothing recommendation
-if current_weather == "sunny":
+if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif current_weather == "rainy":
+elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif current_weather == "cold":
+elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
-elif current_weather != ["sunny", "rainy", "cold"]:
+else:
     print("Sorry, I don't have recommendations for this weather.")
