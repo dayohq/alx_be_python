1da039d87fab2d7c26ce86f7cac97d70c387fa5d
dayohq <smartjoe59@gmail.com>
2025-01-31 15:48:23 +0100

    Mastering Python magic methods

---
+++ oop/book_class.py
@@ -0,0 +1,18 @@
+class Book:
+    def __init__(self, title, author, year):
+        """Initialize a Book instance with title, author, and year."""
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __del__(self):
+        """Prints a message when the Book instance is deleted."""
+        print(f"Deleting {self.title}")
+
+    def __str__(self):
+        """Returns a string representation of the book."""
+        return f"{sefl.title} by {self.author}, published in {self.year}"
+    
+    def __repr__(self):
+        """Returns an official string representation of the book."""
+        return f"Book('{self.title}', '{self.author}', {self.year})"
