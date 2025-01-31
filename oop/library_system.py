83d9317f1c20b425dc68b2f28382727fd7ad9af3
dayohq <smartjoe59@gmail.com>
2025-01-31 16:15:14 +0100

    2nd modification to inheritance and composition exercise.

--- oop/library_system.py
+++ oop/library_system.py
@@ -6,7 +6,7 @@ class Book:
         self.author = author
 
     def __str__(self):
-        return f"Book: {self.title} by {self.title}"
+        return f"Book: {self.title} by {self.author}"
     
 # Derived Class - EBook
 class EBook(Book):
